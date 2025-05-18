cb1a76295fc1531545a1ecd87d7123d8f188a276
Amer Mohamed <96571594+Amermob@users.noreply.github.com>
2025-05-18 07:48:38 +0300

    Update Projects.jsx

--- src/components file/Projects.jsx
+++ src/components file/Projects.jsx
@@ -1,17 +1,24 @@
 export default function Projects(props) {
-
   return (
-    <div className="project">
-      <h2>{props.title}</h2>
-      <img src={props.img} alt={props.alt} />
-      <div className="text-section">
-        <a target="_blank" href={props.link}>
-          Live Demo
-        </a>
-        <a target="_blank" href={props.code}>
-          Source Code
-        </a>
+    <>
+      <div className="project">
+        <h2>{props.title}</h2>
+        <img src={props.img} alt={props.alt} />
+        <div className="text-section">
+          <a target="_blank" href={props.link}>
+            Live Demo
+          </a>
+          <a target="_blank" href={props.code}>
+            Source Code
+          </a>
+        </div>
+        <div className="skills">
+          <img src={props.html} alt="HTML" />
+          <img src={props.css} alt="CSS" />
+          <img src={props.js} alt="JS" />
+          {props.react && <img src={props.react} alt="React" />}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
