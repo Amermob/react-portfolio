b249265548656741ada6dc30bfac1c62ff4b605e
Amer Mohamed <96571594+Amermob@users.noreply.github.com>
2025-06-18 19:56:51 +0300

    Update DarkLight.jsx

--- src/components file/DarkLight.jsx
+++ src/components file/DarkLight.jsx
@@ -1,14 +1,18 @@
-export default function DarkLight(props) {
+import { useRef } from "react";
+
+export default function DarkLight() {
+  const ball = useRef()
+
   function addDarkMod() {
     document.querySelector("body").classList.toggle("dark");
-    document.querySelectorAll(".ball")[0].classList.toggle("handel-ball");
-    document.querySelectorAll(".ball")[1].classList.toggle("handel-ball");
+    ball.current.classList.toggle("handel-ball");
     document.querySelector("header").classList.toggle("dark");
   }
+  
   return (
     <div onClick={addDarkMod} className="dark-mod">
       <div className="ball-container">
-        <div className="ball"></div>
+        <div ref={ball} className="ball"></div>
       </div>
     </div>
   );
